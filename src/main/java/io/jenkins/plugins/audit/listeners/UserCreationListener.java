ffd0c8a97a2947fd21d78c8fafa99899d0516b44
David Olorundare <david.o@ieee.org>
2019-01-08 10:03:28 -0600

    Refactor UserCreationListener class

--- src/main/java/io/jenkins/plugins/audit/listeners/UserCreationListener.java
+++ src/main/java/io/jenkins/plugins/audit/listeners/UserCreationListener.java
@@ -1,61 +1,18 @@
 package io.jenkins.plugins.audit.listeners;
 
-
-import hudson.Extension;
-
-import hudson.ExtensionList;
-import hudson.Extension;
-import io.jenkins.plugins.audit.RequestContext;
-import jenkins.security.SecurityListener;
-
-import javax.annotation.Nonnull;
-import java.io.IOException;
-import java.time.Instant;
-
-import io.jenkins.plugins.audit.event.CreateUser;
-import org.acegisecurity.userdetails.User;
 import org.apache.logging.log4j.audit.LogEventFactory;
-import org.acegisecurity.userdetails.UserDetails;
+
+import hudson.Extension;
+import hudson.ExtensionList;
+import io.jenkins.plugins.audit.event.CreateUser;
+import javax.annotation.Nonnull;
+import jenkins.security.SecurityListener;
 
 /**
  * Listener notified of user creation events.
  */
 @Extension
 public class UserCreationListener extends SecurityListener {
-
-    /**
-     * Fired when a user was successfully authenticated using credentials. It could be password or any other credentials.
-     * This might be via the web UI, or via REST (using API token or Basic), or CLI (remoting, auth, ssh)
-     * or any other way plugins can propose.
-     *
-     * @param details details of the newly authenticated user, such as name and groups.
-     */
-    protected void authenticated(@Nonnull UserDetails details) {}
-
-    /**
-     * Fired when a user has failed to log in.
-     * Would be called after {@link #failedToAuthenticate}.
-     *
-     * @param username the user
-     */
-    protected void failedToLogIn(@Nonnull String username) {}
-
-    /**
-     * Fired when a user tried to authenticate but failed.
-     * UserLogListener does not use this abstract class method, however, rather than leaving
-     * its implementation empty it is made to throw an exception if the method is ever called.
-     *
-     * @param username the user
-     * @see #authenticated
-     */
-    protected void failedToAuthenticate(@Nonnull String username) {}
-
-    /**
-     * Fired when a user has logged in, event logged via Log4j-audit.
-     *
-     * @param username name or ID of the user who logged in.
-     */
-    protected void loggedIn(@Nonnull String username) {}
 
     /**
      * Fired when a new user account has been created.
@@ -71,16 +28,11 @@ public class UserCreationListener extends SecurityListener {
     }
 
     /**
-     * Fired when a user has logged out, event logged via Log4j-audit.
-     *
-     * @param username name or ID of the user who logged out.
+     * Returns all the registered {@link UserCreationListener}s.
      */
-    protected void loggedOut(@Nonnull String username) {}
-
-    /**
-     * Returns all the registered {@link UserLogListener}s.
-     */
-    public static ExtensionList<UserCreationListener> all() {
+    public static ExtensionList<UserCreationListener> getInstance() {
         return ExtensionList.lookup(UserCreationListener.class);
     }
+
+
 }
