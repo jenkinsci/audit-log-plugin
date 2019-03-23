a7ccf6289382f5c72d67471fad11a196058ec7b7
Radhika <radhika@cs.iitr.ac.in>
2019-03-23 14:04:10 +0530

    Modify date format to use current timezone

--- src/main/java/io/jenkins/plugins/audit/helpers/Factory.java
+++ src/main/java/io/jenkins/plugins/audit/helpers/Factory.java
@@ -4,13 +4,17 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class Factory {
     /*
      * Returns a date string in the ISO-8601 format
      */
     public static String dateTimeFormat() {
-        ZonedDateTime d = ZonedDateTime.of(LocalDateTime.now(), ZoneId.of("America/Los_Angeles"));
+
+    	TimeZone tz = Calendar.getInstance().getTimeZone();
+        ZonedDateTime d = ZonedDateTime.of(LocalDateTime.now(), ZoneId.of(tz.getID()));
         String s = d.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
         return s;
     }
